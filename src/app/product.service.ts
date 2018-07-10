79f2a42465e60eacba47f6903834f9fd583af6ff
RogerMoon <rogerluna95@hotmail.com>
2018-07-10 15:47:46 -0500

    bla 11

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,11 +5,14 @@ import { Album } from './album';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 
+import { Product } from './product';
+
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -17,6 +20,15 @@ export class ProductService {
 
     return this._http.get(this._albumUrl).map((response) =>
     <Album>response.json());
+
+  }
+
+  getProducts(): Observable<Product[]> {
+
+    return this._http.get(this._productsUrl).map(response =>
+    <Product[]>response.json());
+
+
 
   }
 
